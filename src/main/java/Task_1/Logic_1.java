daf750b4c26023f717d7aa813a3aa0c5a7823c78
RaguCS <ragusfc143@gmail.com>
2025-02-01 01:03:37 +0530

    pushed

--- src/main/java/Task_1/Logic_1.java
+++ src/main/java/Task_1/Logic_1.java
@@ -1,4 +1,10 @@
 package Task_1;
 
 public class Logic_1 {
+    public boolean cigarParty(int cigars, boolean isWeekend) {
+        if(!isWeekend&&cigars>=40&&cigars<=60)return true;
+        else
+            return isWeekend && cigars >= 40;
+    }
+
 }
