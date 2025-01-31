66181b868703055d8e3fbc27af0bea334d06ee7a
RaguCS <ragusfc143@gmail.com>
2025-02-01 02:55:56 +0530

    pushed

--- src/main/java/Task_1/Logic_1.java
+++ src/main/java/Task_1/Logic_1.java
@@ -6,5 +6,11 @@ public class Logic_1 {
         else
             return isWeekend && cigars >= 40;
     }
+    public int dateFashion(int you, int date) {
+        if((you>7&&date>2)||(date>7&you>2))return 2;
+        else
+            return you < 3 || date < 3 ?0:1;
+    }
+
 
 }
