edb2bae57bfe2859192d6cfbf89fa52702a19a3c
RaguCS <ragusfc143@gmail.com>
2025-03-20 17:06:01 +0530

    pushed

---
+++ src/main/java/com/ragu/A.java
@@ -0,0 +1,10 @@
+package com.ragu;
+
+public class A {
+void m1(){}
+}
+class B extends A{
+    void m1(){
+        System.out.println("m1");
+    }
+}
--- src/main/java/com/ragu/Main.java
+++ src/main/java/com/ragu/Main.java
@@ -4,8 +4,6 @@ import java.util.*;
 
 public class Main {
 
-
-
 //    public static int maximumGap(int[] nums) {
 //        if (nums == null || nums.length < 2) {
 //            return 0;
@@ -92,12 +90,33 @@ public int findMin(int[] nums) {
     return result;
     }
 
+    public List<String> restoreIpAddresses(String s) {
+        List<String> ans = new ArrayList<>();
+        recursive("", 0, 0, s, ans);
+        return ans;
+    }
+
+    void recursive(String path, int index, int dots, String str, List<String> ans) {
+        if (dots > 4) return;
+        if (dots == 4 && index >= str.length()) {
+            ans.add(path.substring(0,path.length()-1));
+            return;
+        }
+        for (int length = 1; length <= 3 && index + length <= str.length(); length++) {
+            String num = str.substring(index, index + length);
+            if (num.charAt(0) == '0' && length != 1) break;
+            else if (Integer.parseInt(num) <= 255) {
+                recursive( path + str.substring(index, index + length) + ".", index + length,dots + 1, str, ans);
+            }
+        }
+    }
+
 
 public static void main(String[] args) {
-        Main main=new Main();
-        String[] arr={"aba","bcb","ece","aa","e"};
-        int[][] queries={{0,2},{1,4},{1,1}};
-        System.out.println(Arrays.toString(main.vowelStrings(arr,queries)));
+    String s="25525511135";
+//    System.out.println(s.compareTo());
+    System.out.println(new Main().restoreIpAddresses(s));
+
     }
     public List<String> generateParentheses(int n){
     List<String> list=new ArrayList<>();
